557cbc5ab4773f1318a80b318d882bd99d313d84
4ferre <dfgergx@laposte.net>
2018-08-20 14:25:27 +0200

    post get

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -1,28 +1,40 @@
 <?php
 namespace App\Controller;
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\FOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
 class UsersController extends FOSRestController
 {
 
-
+    private $em;
     private $userRepository;
-    public function __construct(UserRepository $userRepository)
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $em)
     {
         $this->userRepository = $userRepository;
+        $this->em = $em;
     }
-    public function getUsersAction()
+    public function getUsersAction(User $user)
     {
-        $users = $this->userRepository->findAll();
-        return $this->view($users);
+       // $users = $this->userRepository->findAll();
+        return $this->view($user);
     }
 
 
+    /**
+     * @Rest\Post("/users")
+     * @ParamConverter("user", converter="fos_rest.request_body")
+     */
+    public function postUsersAction(User $user, EntityManagerInterface $em)
+    {
+        $this->em->persist($user);
+        $this->em->flush();
+        return $this->view($user);
+    }
 
-    public function getUserAction($id)
-    {} // "get_user" [GET] /users/{id}
-    public function postUsersAction()
-    {} // "post_users" [POST] /users
     public function putUserAction($id)
     {} // "put_user" [PUT] /users/{id}
     public function deleteUserAction($id)
