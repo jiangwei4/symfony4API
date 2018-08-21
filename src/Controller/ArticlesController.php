58b169c894e02950260d6515bb66c8c0a06c8ce1
4ferre <dfgergx@laposte.net>
2018-08-21 11:19:29 +0200

    crud article

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -1,38 +1,68 @@
 <?php
 namespace App\Controller;
-use App\Entity\User;
+
 use App\Entity\Article;
+use App\Repository\ArticlesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
 
 class ArticlesController extends FOSRestController
 {
+    private $articlesRepository;
     private $em;
-    public function __construct( EntityManagerInterface $em)
+    public function __construct(ArticlesRepository $articlesRepository, EntityManagerInterface $em)
     {
+        $this->articlesRepository = $articlesRepository;
         $this->em = $em;
     }
-    public function getArticlesAction(Article $article)
+    /**
+     * @Rest\View(serializerGroups={"article.user"})
+     */
+    public function getArticlesAction()
+    {
+        $articles = $this->articlesRepository->findAll();
+        return $this->view($articles);
+    }
+    /**
+     * @Rest\View(serializerGroups={"article.user"})
+     */
+    public function getArticleAction(Article $article)
     {
         return $this->view($article);
     }
-    public function getArticleAction(int $id)
+    /**
+     * @Rest\Post("/articles")
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function postArticlesAction(Article $article)
     {
-    }
-    public function postArticlesAction(Article $article,Request $request, EntityManagerInterface $em)
-    {
-
-        $article->setUser($request->getUser());
+        $article->setUser($this->getUser());
         $this->em->persist($article);
         $this->em->flush();
-        return new JsonResponse('yes');
+        return $this->view($article);
     }
-    public function putArticleAction(int $id)
+    public function putArticleAction(int $id, Request $request)
     {
+        $tl = $request->get('title');
+        $dp = $request->get('description');
+        $article = $this->articlesRepository->find($id);
+        if ( $tl ){
+            $article->setTitle($tl);
+        }
+        if ( $dp ){
+            $article->setDescription($dp);
+        }
+        $this->em->persist($article);
+        $this->em->flush();
+        return $this->view($article);
     }
-    public function deleteArticleAction(int $id)
+    public function deleteArticleAction(Article $article)
     {
+        $this->em->remove($article);
+        $this->em->flush();
     }
 }
--- src/Repository/ArticlesRepository.php
+++ src/Repository/ArticlesRepository.php
@@ -1,26 +1,22 @@
 <?php
-
 namespace App\Repository;
-
-use App\Entity\Articles;
+use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-
 /**
- * @method Articles|null find($id, $lockMode = null, $lockVersion = null)
- * @method Articles|null findOneBy(array $criteria, array $orderBy = null)
- * @method Articles[]    findAll()
- * @method Articles[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Article|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Article|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Article[]    findAll()
+ * @method Article[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ArticlesRepository extends ServiceEntityRepository
 {
     public function __construct(RegistryInterface $registry)
     {
-        parent::__construct($registry, Articles::class);
+        parent::__construct($registry, Article::class);
     }
-
 //    /**
-//     * @return Articles[] Returns an array of Articles objects
+//     * @return Article[] Returns an array of Article objects
 //     */
     /*
     public function findByExampleField($value)
@@ -35,9 +31,8 @@ class ArticlesRepository extends ServiceEntityRepository
         ;
     }
     */
-
     /*
-    public function findOneBySomeField($value): ?Articles
+    public function findOneBySomeField($value): ?Article
     {
         return $this->createQueryBuilder('a')
             ->andWhere('a.exampleField = :val')
